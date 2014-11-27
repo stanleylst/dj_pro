846941f9980dd0b2fc87fe9125e3f459caca5c37
your name <email address>
2014-11-28 07:31:42 +0800

    add music multi-user control and tidy the logic of back-script

--- dj_pro/dj_pro/static/js/angular-controller/PlayerController.js
+++ dj_pro/dj_pro/static/js/angular-controller/PlayerController.js
@@ -1,17 +1,37 @@
 app.controller('PlayerController',
   function($scope,Music) {
     $scope.musics = Music.query();
-    Music.query(function(musics){             //对query的值处理一定要放在这里
+    Music.query(function(musics){             //put on where values of query is going to deal with ;
     $scope.mymusic = musics;                         
+    $scope.allmusiclist = [];
+    for (i in $scope.mymusic){                                  //aquire musiclist array
+        $scope.allmusiclist.push({id:i,owner: $scope.mymusic[i].user,src:'/media/'+$scope.mymusic[i].music_file, type : "audio/ogg",img:'/media/'+$scope.mymusic[i].music_img});
+    };
+
+    $scope.music_owner = 'root';                                  //change the musiclist of owner;
+    $scope.ownerchange = function(owner){
+        $scope.music_owner = owner.username;
+        console.log(owner.username);
+    };
+
+    $scope.$watch('music_owner',function() {
     $scope.musiclist = [];
-    for (i in $scope.mymusic)                                  //取得musiclist数组   
-        $scope.musiclist.push({id:i,owner: $scope.mymusic[i].username,src:'/media/'+$scope.mymusic[i].music_file, type : "audio/ogg",img:'/media/'+$scope.mymusic[i].music_img});
+    for (i in $scope.allmusiclist){                             //choose the musiclist via owner;
+        console.log($scope.allmusiclist[i].owner);
+        if ($scope.allmusiclist[i].owner == $scope.music_owner){
+            $scope.musiclist.push($scope.allmusiclist[i]);
+        };
+    };
+
     $scope.playlist= $scope.musiclist;
 
+    console.log($scope.playlist);
     $scope.sum_num = parseInt($scope.playlist[$scope.playlist.length-1].id)+1;
-    
+
+    $scope.audio.play(0);
+    });
  
-    $scope.repeat = false;
+    $scope.repeat = false;                                   //the following is all to solve repeating a song
     $scope.canturn = true;
     $scope.repeat_func = function(){
             $scope.repeat = !$scope.repeat;
@@ -26,7 +46,7 @@ app.controller('PlayerController',
         if($scope.repeat == true){
             $scope.audio.next();
             $scope.canturn = false;
-            setTimeout(canturn,200);
+            setTimeout(canturn,20);
             }else{$scope.audio.next();};
     };      
     
@@ -35,10 +55,10 @@ app.controller('PlayerController',
         if($scope.repeat == true){
             $scope.audio.prev();
             $scope.canturn = false;
-            setTimeout(canturn,200);
+            setTimeout(canturn,20);
             }else{$scope.audio.prev();};
     };      
-
+    
     $scope.$watch('audio.ended',function() {
         console.log($scope.audio.ended+'    '+$scope.audio.currentTrack);
         console.log('go here?');
@@ -51,7 +71,13 @@ app.controller('PlayerController',
             console.log('last song');
             $scope.audio.play();
             $scope.canturn = false;
-            setTimeout(canturn,200);
+            setTimeout(canturn,20);
+        };
+        console.log('end watch');
+        console.log($scope.audio.ended+'    '+$scope.audio.currentTrack);
+        if( curr_num == -1){
+            console.log('first');
+            $scope.audio.play(0);
         };
 
     });
--- dj_pro/dj_pro/static/js/angular-controller/game_script_ctrl.js
+++ dj_pro/dj_pro/static/js/angular-controller/game_script_ctrl.js
@@ -6,14 +6,35 @@ app.controller('game_script_ctrl', function($scope, Game_Script) {
         $scope.gamename = game_scripts;
         $scope.gamename_len = [];
         //开始去重
-        for (i in $scope.gamename)                                  //取得gamename数组     
-            $scope.gamename_len.push($scope.gamename[i].gamename);
+        for (i in $scope.gamename){                                  //取得gamename数组     
+            $scope.gamename_len.push($scope.gamename[i].gamename);};
         $scope.gamename_uniq = [];
         $.each($scope.gamename_len, function(i, el){                  //取得去重数组
             if($.inArray(el, $scope.gamename_uniq) === -1) $scope.gamename_uniq.push(el);
         }); 
+        
+        $scope.gamename_sum_ip = [];
+        $scope.gamename_use_has_ip = [];
+        for (i in $scope.gamename){                                  //取得gamename数组   
+                $scope.gamename_sum_ip.push($scope.gamename[i].access_ip);
+                $scope.gamename_use_has_ip.push($scope.gamename[i].gamename+$scope.gamename[i].access_ip);
+                };
+                $scope.gamename_uniq_ip = [];
+                $.each($scope.gamename_sum_ip, function(i, el){                  //取得去重数组
+                    if($.inArray(el, $scope.gamename_uniq_ip) === -1) $scope.gamename_uniq_ip.push(el);
+                }); 
+
+        $scope.has_ip = function(script,ip){
+            combine = script+ip;
+            for(i in $scope.gamename_use_has_ip){
+                if(combine == $scope.gamename_use_has_ip[i])
+                    {return true};
+
+                };
+            };
       
         }); 
 
-    $scope.isCollapsed = true;
+    $scope.isCollapsed1 = true;
+    $scope.isCollapsed2 = true;
 });    
