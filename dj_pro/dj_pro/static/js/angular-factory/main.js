11a04c783f49f8e93993ec034ae528df21dea055
your name <email address>
2014-12-22 17:27:55 +0800

    looking for other change

---
+++ dj_pro/dj_pro/static/js/angular-factory/main.js
@@ -0,0 +1,14 @@
+var app = angular.module('myApp', ['mediaPlayer','ngResource','ngCookies','ui.bootstrap','angular-md5','base64']);
+    app.config(function($interpolateProvider) { 
+      $interpolateProvider.startSymbol('((');
+      $interpolateProvider.endSymbol('))');
+    });
+    app.run(
+    function($http, $cookies) {
+        $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
+        // Add the following two lines
+        $http.defaults.xsrfCookieName = 'csrftoken';
+        $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+    });
+
+
